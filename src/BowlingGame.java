98823482ec20863b44b41de40be5a6075682e7df
gonzalobestoso <gonzalojavier19@gmail.com>
2017-11-27 18:02:48 -0300

    tirar nada anda

--- src/BowlingGame.java
+++ src/BowlingGame.java
@@ -1,12 +1,13 @@
 
 public class BowlingGame {
+	private int puntos =0;
 	
 	public void tirar(int pinos){
-		
+		puntos = pinos;
 	}
 	
 	public int getScore(){
-		return 1;
+		return puntos;
 	}
 
 }
--- src/BowlingGameTest.java
+++ src/BowlingGameTest.java
@@ -15,5 +15,4 @@ public class BowlingGameTest {
 		assertEquals(0,juego.getScore());
 	}
 
-	
 }
